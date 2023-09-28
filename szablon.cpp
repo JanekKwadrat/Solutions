f99f98bb45b7dda16b1a3187ff43dcbd6c37e9f7
JanekKwadrat <janek.kwadrat@gmail.com>
2023-09-28 07:22:57 +0200

    Dodano szablon

---
+++ szablon.cpp
@@ -0,0 +1,19 @@
+/*  Jan Zakrzewski
+    --- */
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int N = 1;
+int t;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> t;
+    while(t--) {
+
+    }
+
+    return 0;
+}
